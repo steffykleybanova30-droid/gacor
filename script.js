9377dc045d814227de92b8581784c4559c3fb1bb
steffy <steffykleybanova30@gmail.com>
2026-04-10 13:07:14 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+// Toggle class active
+const navbarNav = document.querySelector('.navbar-nav');
+// Ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => {
+    navbarNav.classList.toggle('active');
+
+hamburgerMenu.addEventListener('click', () => {
+  navbarNav.classList.toggle('active');
+});
