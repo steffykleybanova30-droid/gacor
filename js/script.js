bfe8b3c80a80554c839048fda0fa4297185e26ef
steffy <steffykleybanova30@gmail.com>
2026-06-18 09:50:23 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -7,17 +7,12 @@ hamburger.onclick = (e) => {
   navbarNav.classList.toggle("active");
 };
 
-// Gabungkan semua fungsi klik di dokumen jadi satu
 document.addEventListener("click", function (e) {
-  // 1. Logika untuk menutup Dropdown
-  // Jika yang diklik BUKAN elemen .dropdown, maka semua dropdown ditutup
   if (!e.target.closest(".dropdown")) {
     document.querySelectorAll(".dropdown").forEach((d) => {
       d.classList.remove("active");
     });
   }
-
-  // 2. Logika untuk menutup Hamburger Menu
   if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove("active");
   }
@@ -29,13 +24,9 @@ document.querySelectorAll(".dropbtn").forEach((btn) => {
     e.stopPropagation();
 
     const dropdown = btn.parentElement;
-
-    // tutup dropdown lain
     document.querySelectorAll(".dropdown").forEach((d) => {
       if (d !== dropdown) d.classList.remove("active");
     });
-
-    // toggle yang diklik
     dropdown.classList.toggle("active");
   });
 });
@@ -46,31 +37,25 @@ document.addEventListener("click", function () {
   });
 });
 
+
 // ANIMASI FOTO MEMBER //
 const fotoMember = document.querySelectorAll(".member-card-img");
 const sectionMember = document.querySelector(".about-member");
 
 window.addEventListener("scroll", () => {
-  // Mengambil posisi koordinat kotak section member relatif terhadap layar
   const posisiSection = sectionMember.getBoundingClientRect();
-
-  // 1. JIKA SCROLL TURUN: Section member mulai masuk ke area layar bawah
   if (posisiSection.top < window.innerHeight - 150) {
     fotoMember.forEach((foto, indeks) => {
-      // Hanya jalankan transisi jika fotonya belum muncul
       if (!foto.classList.contains("sudah-muncul")) {
         setTimeout(() => {
           foto.classList.add("sudah-muncul");
-        }, indeks * 250); // Jeda antrean antar-foto (0.25 detik)
+        }, indeks * 250); 
       }
     });
   }
-
-  // 2. JIKA SCROLL NAIK MENTOK: Layar naik ke atas sampai section member bener-bener gak kelihatan di bawah
-  // Ini syarat biar kalau scroll naik biasa dia TETEP DI SITU, tapi kalau naik banget sampai lewat atas baru ke-reset
   if (posisiSection.top > window.innerHeight) {
     fotoMember.forEach((foto) => {
-      foto.classList.remove("sudah-muncul"); // Reset jadi transparan lagi buat transisi berikutnya
+      foto.classList.remove("sudah-muncul");
     });
   }
 });
